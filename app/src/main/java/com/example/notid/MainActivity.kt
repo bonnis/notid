cfb8a53aae49ac6c0e40b66ca2920c02e3f850f5
bonnis <bonnis1st>
2020-04-10 07:14:06 +0700

    Fragment using NavUI(Jetpack)

--- app/src/main/java/com/example/notid/MainActivity.kt
+++ app/src/main/java/com/example/notid/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Button
+import com.facebook.appevents.codeless.internal.ViewHierarchy.setOnClickListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,12 +16,21 @@ class MainActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         //this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+        val loginBtn: Button = findViewById(R.id.login_btn)
+        loginBtn.setOnClickListener { v -> login(v) }
+        val regBtn: Button = findViewById(R.id.regBtn)
+        regBtn.setOnClickListener { v ->
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+         }
     }
 
-    fun login(view: View){
+    private fun login(view: View){
         val intent = Intent(this, NotesActivity::class.java).apply {
             putExtra("com.example.main.Sample", "Wacemeh")
         }
         startActivity(intent)
     }
+
+
 }
--- app/src/main/java/com/example/notid/NotesActivity.kt
+++ app/src/main/java/com/example/notid/NotesActivity.kt
@@ -3,24 +3,55 @@ package com.example.notid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import android.view.View
-import android.widget.Toolbar
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class NotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes)
-        val toolbar : androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
+        toolbar.title = null
+        val navController = findNavController(R.id.fragment_container)
+        val title:TextView = findViewById(R.id.toolbar_title)
         val drawer : DrawerLayout = findViewById(R.id.drawer_layout)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawer)
+        findViewById<NavigationView>(R.id.nav_view)
+            .setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.label.toString() == "fragment_note"){
+                title.text = "My Notes"
+            } else if(destination.label.toString() == "fragment_history"){
+                title.text = "Access History"
+            }
+        }
+
         val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open, R.string.nav_close)
         drawer.addDrawerListener(toggle)
         toggle.syncState()
+
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.v("ItemMenuTrigger","Triggered!"+item.itemId)
+        val navController = findNavController(R.id.fragment_container)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
 
 }
